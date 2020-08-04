dcf1bf263e8f31cb97559e41dd08264685b2e629
Pedro HIB <pedrohizib@gmail.com>
2020-08-03 23:04:09 -0300

    Inicio de criaação do Layout

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/conteudo.css">
+    <title>Panda's Lanches</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <img class="logo" src="img/logo.png"/>
+        <h1>Panda's Lanche</h1>
+    </header>
+
+
+    <nav class="navegacao">
+        <a href="delivery.php">Delivery <img src="img/delivery.png"></a>
+        <a href="promocao.php" class="promocao" class="azul">Promoção</a>
+    </nav>
+
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+
+            </nav>        
+        </div>
+    </main>
+
+
+    <footer class="rodape">
+        <a href="https://api.whatsapp.com/send?phone=5522998935802&text=Boa%20noite!">
+            <img src="img/whatsapp.png"/>
+        </a>
+        <a href="https://www.instagram.com/pandaslanches/">
+            <img src="img/instagram.png"/>
+        </a>  
+    </footer>
+</body>
+</html>
